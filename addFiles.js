5d4e0a429e0674ba0348b7871799888d9333c45c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-02-21 11:50:03 +0300

    delete addFiles

--- addFiles.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-function addFiles(dirName, fileName) {
-  fs.mkdirSync(dirName);
-  fs.writeFileSync(`${dirName}/${fileName}.ts`, "");
-  fs.writeFileSync(`${dirName}/${fileName}.js`, "");
-  fs.writeFileSync(`${dirName}/${fileName}.py`, "");
-  fs.writeFileSync(`${dirName}/${fileName}.go`, "");
-}
